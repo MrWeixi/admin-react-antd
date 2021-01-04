a2ab041ba6539a6ab43d6ad3e0183e9a4bb3caa0
魏自朋 <wzps789.@163.com>
2021-01-04 18:07:26 +0800

    路由嵌套相关

--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -1,32 +1,39 @@
 
 const menuData = [
-    {
-        title: "首页",
-        path: "/dashboard",
-        icon: "home",
-        roles: ["admin","guest"]
-    },
-    {
-        title: "表格",
-        path: "/table",
-        icon: "table",
-        roles: ["admin", "editor"]
+  {
+    title: "首页",
+    path: "/dashboard",
+    icon: "home",
+    roles: ["admin", "guest"]
+  },
+  {
+    title: "表格",
+    path: "/table",
+    icon: "table",
+    roles: ["admin", "editor"]
+  },
+  {
+    title: "路由嵌套",
+    path: "/nested",
+    icon: "cluster",
+    roles: ["admin", "editor"],
+    children: [
+      {
+        title: "菜单1",
+        path: "/nested/menu1"
       },
       {
-        title: "路由嵌套",
-        path: "/nested",
-        icon: "cluster",
-        roles: ["admin", "editor"],
+        title: "菜单2",
+        path: "/nested/menu2",
         children: [
           {
-            title: "菜单1",
-            path: "/nested/menu1"
-          },
-          {
-            title: "菜单2",
-            path: "/nested/menu2"
-          },
-        ],
+            title: "菜单2-2",
+            path: "/nested/menu2/menu2-2",
+            roles: ["admin", "editor"]
+          }
+        ]
       },
+    ],
+  },
 ]
 export default menuData
