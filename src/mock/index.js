4b5ff93ed703303474f90c92f43161e305c39b54
魏自朋 <wzps789.@163.com>
2021-01-05 18:08:11 +0800

    优化表单

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,9 +1,13 @@
 import Mock from "mockjs";
 // 模拟数据
 import loginAPI from "./login";
-
+import tableAPI from "./table";
 // 登录用户相关信息
 
 Mock.mock(/\/login/, "post", loginAPI.login)
 Mock.mock(/\/userInfo/, "post", loginAPI.userInfo)
 Mock.mock(/\/logout/, "post", loginAPI.logout)
+// table
+Mock.mock(/\/table\/list/, "post", tableAPI.tableList);
+Mock.mock(/\/table\/delete/, "post", tableAPI.deleteItem);
+Mock.mock(/\/table\/edit/, "post", tableAPI.editItem);
--- src/views/Table/index.jsx
+++ src/views/Table/index.jsx
@@ -1,10 +1,35 @@
-import React from "react"
+import React, { Component } from "react"
+import { tableList, deleteItem, editItem } from "@/api/table";
 
-const Table=()=>{
-    return(
-        <div>
-            表格
-        </div>
-    )
+class TableGrid extends Component {
+    state = {
+        list: [],
+        loading: false,
+        total: 0,
+        listQuery: {
+            pageNumber: 1,
+            pageSize: 10,
+            title: "",
+            star: "",
+            status:""
+          },
+    }
+    // 取得数据
+    fetchData = () => {
+        tableList(this.state.listQuery).then(resJson => {
+            console.log(resJson)
+        })
+    }
+    componentDidMount(){
+    this.fetchData();
+    }
+    render() {
+        return (
+            <div>
+                表格
+            </div>
+        )
+    }
+
 }
-export default Table
+export default TableGrid
